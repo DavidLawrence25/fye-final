54884182595996c7b764eeb3b8d25b9f7c2bfc6d
Aaron Night <mdcartkids@gmail.com>
2023-01-06 15:39:50 -0700

    Heading Prep

--- buttons.js
+++ buttons.js
@@ -1,3 +1,4 @@
-function changePage(page) { // note: don't include ".html"
-    window.location.href = `${page}.html`
+function scrollToHeading(heading) {
+	let headingY = document.getElementById(`${heading}`).getBoundingClientRect().top
+	scrollTo(0, headingY);
 }
